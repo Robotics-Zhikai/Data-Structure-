84ad515f4b74bd0c28f2d56b362f608e7b02bd6c
LAPTOP\zhikaiwang <979256120@qq.com>
2021-02-28 11:07:33 +0800

    LeetCode Dynamic Programming 剑指Offer14 剪绳子II 溢出大数求余

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -2,12 +2,113 @@
 #include "main.h"
 
 /*
+剑指 Offer 14- II. 剪绳子 II
+给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+每段绳子的长度记为 k[0],k[1]...k[m - 1] 。请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？
+例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+*/
+
+class Solution14_II {
+public:
+
+	/*
+	int cuttingRope(int n) {
+	//如果任旧用剪绳子I中的方法计算，则在中间的计算过程中就会越界 所以在数值有限的情况下不能用这种方法
+	n=120;
+	vector<long> Tn(n + 1, 0);
+	if (n == 0)
+	return 0;
+	else if (n == 1)
+	{
+	Tn[1] = 1;
+	return 1;
+	}
+	Tn[1] = 1;
+
+	for (long In = 2; In <= n; In++) //时间复杂度是N^2 空间复杂度是N 有巧妙的方法能降到N  但是估计面试是想不到的
+	{
+	long maxthis = 0;
+	for (long i = 1; i <= In - 1; i++)
+	{
+	long big = max(max(Tn[i] * (In - i), i*(In - i)), Tn[i] * Tn[In - i]);//注意这个判断容易丢
+	//在这里容易出现越界
+	//动态规划的核心
+	if (big>maxthis)
+	{
+	maxthis = big;
+	}
+	}
+	Tn[In] = maxthis;
+	}
+	return Tn[n]%1000000007;
+	}
+	*/
+
+	long bigmodc(long x, long a, long c) //大数的求余
+	{
+		//x^a % c = ((x^(a-1)%c)*(x%c))%c
+		long xmodc = x%c;
+		long result = 1;
+		for (int i = 1; i <= a; i++)
+		{
+			result = ((result%c)*xmodc) % c;
+		}
+		return result;
+	}
+	int cuttingRope(int n) //0ms 5.8MB 100% 69.42%
+	{
+		/*
+		先验知识：将绳子尽量等分时得到的乘积最大 再经过推导绳子等分成每段长度为3时长度最长
+		对任意长度的绳子，对3取模会余0 1 2
+		余0时直接计算就行
+		余1时拆分一个3再加上1为2+2
+		余2时不拆分，直接乘2即可
+		这其实是另一种形式的动态规划。每次的最优值由前一次的最优值递推得到
+		递推是常数时间
+		(x*y)%c = ((x%c)*(y%c))%c 根据这个式子可以求得大数的模
+		*/
+		if (n == 0)
+			return 0;
+		else if (n == 1 || n == 2)
+			return 1;
+		else if (n == 3)
+			return 2;
+		int a = n / 3;
+		int mod = n % 3;
+
+		long modnum = 1000000007;
+		if (mod == 0)
+		{
+			return bigmodc(3, a, modnum);
+		}
+		else if (mod == 1)
+		{
+			//(3^a-1 * 4)%modnum
+			return (bigmodc(3, a - 1, modnum) * 4) % modnum;
+		}
+		else if (mod == 2)
+		{
+			//(3^a * 2)%modnum
+			return (bigmodc(3, a, modnum) * 2) % modnum;
+		}
+		return -1;
+	}
+};
+
+
+
+
+
+
+/*
 剑指 Offer 14- I. 剪绳子
 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
 */
-class SolutionOffer14_1 {
+class SolutionOffer14_I {
 public:
 	int cuttingRope(int n) {
 		vector<int> Tn(n + 1, 0);
