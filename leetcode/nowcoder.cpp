e860497520626a8b5a249fa94a544282d189498a
LAPTOP\zhikaiwang <979256120@qq.com>
2021-04-10 17:03:25 +0800

    leetcode nowcoder input output practice

---
+++ leetcode/nowcoder.cpp
@@ -0,0 +1,35 @@
+//Å£¿ÍÊäÈëÊä³öÁ·Ï°
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int test()
+{
+	int a, b;
+	cin >> a >> b;
+	while (cin >> a)
+	{
+		if (cin.get() == '\n')
+		{
+
+		}
+	}
+	char c;
+	while (cin.get(c))
+	{
+		if (c == '\n')
+		{
+
+		}
+		if (c == ',')
+		{
+
+		}
+	}
+	string str;
+	cin >> str;
+	str += "s";
+	return 1;
+}
