3641299fd745b5c4daedf34a46e2cda3ccea86e3
LAPTOP\zhikaiwang <979256120@qq.com>
2021-05-21 10:59:40 +0800

    剑指offer 55_1 二叉树的深度 DFS 优化子结构

--- leetcode/DFS.h
+++ leetcode/DFS.h
@@ -134,3 +134,25 @@ public:
 		exist(board, "aaaaa");
 	}
 };
+
+/*
+剑指 Offer 55 - I. 二叉树的深度
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	
+};
+class SolutionOffer55_I { //跟动态规划差不多，就是找到优化子结构，然后得到合问题的解
+public:
+	int maxDepth(TreeNode* root) {
+		if (root == nullptr) {
+			return 0;
+		}
+		return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+	}
+};
