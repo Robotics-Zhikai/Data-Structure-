8f80c1781a56156409b40830b92d175d32af97a4
Robotics-Zhikai <979256120@qq.com>
2021-05-24 12:12:55 +0800

    剑指offer 16 数值的整数次方 递归很容易写 迭代不好写

--- leetcode/Recursion.h
+++ leetcode/Recursion.h
@@ -40,3 +40,65 @@ public:
 		// return recur(n)%(int)(pow(10,9)+7);
 	}
 };
+
+/*
+剑指 Offer 16. 数值的整数次方
+实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
+
+*/
+
+class SolutionOffer16 {
+public:
+    double recur(double x,long n)
+    {
+        if (n==1)
+            return x;
+        if (n==0 || x==1)
+            return 1;
+        double tmp = recur(x,n/2); //用主定理可知时间复杂度是logn
+        if (n%2==0)
+            return tmp*tmp;
+        else
+            return tmp*tmp*x;
+    }
+    double myPowRecur(double x,int n) //递归方法做
+    {
+        if(x==0)
+            return 0;
+        if (n<0)
+            // return 1/recur(x,-n); //注意int的第一位是符号位， 导致了正负取值范围不对称，当n=-2^31时，-n仍等于-2^31
+            return recur(1/x,-(long)n);
+        else
+            return recur(x,n);
+    }
+
+    double myPowIterate(double x,int n) //迭代方法做
+    {
+        //当n为偶数，x^n=(x^2)^(n/2)=(x^4)^(n/4)=...
+        //当n为奇数，x^n=x*(x^2)^(n/2)
+        if(x == 0) return 0;
+        long absn = n;
+        if (n<0){
+            absn = -(long)n;
+            x = 1/x;
+        }
+        
+        double res = 1;
+        double curx = x;
+        while(absn>0){
+            if (absn&1){ //说明此时n是奇数 最后一次迭代肯定是奇数
+                res *= curx; //curx就是把x^n=x*(x^2)^(n/2)上一次迭代括号里边的x剥出来
+            }
+            curx = curx*curx;
+            absn>>=1; //absn/2 logn的复杂度
+        }
+        return res;
+    }
+
+    double myPow(double x, int n) {
+        // return myPowRecur(x,n); //用递归的方法空间复杂度是O(n)
+        return myPowIterate(x,n); //用迭代方法的空间复杂度是O(1)
+        //迭代方法比较难写！！！！！！！！！！！！！！！！！！！！！！！
+        //递归方法很好写
+    }
+};
