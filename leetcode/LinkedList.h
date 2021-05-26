dc9ff489515b137b43b27f0c4d10c3a51f06e54d
Robotics-Zhikai <979256120@qq.com>
2021-05-26 18:20:26 +0800

    LeetCode138 剑指offer35 复制复杂链表 DFS和链表方法

--- leetcode/LinkedList.h
+++ leetcode/LinkedList.h
@@ -71,3 +71,123 @@ public:
 
 };
 
+
+/*
+138. 复制带随机指针的链表
+给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+
+构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+
+例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+
+返回复制链表的头节点。
+
+用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+
+val：一个表示 Node.val 的整数。
+random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+你的代码 只 接受原链表的头节点 head 作为传入参数。
+*/
+
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+class Solution138 {
+public:
+    Node* FirstThought(Node * head) //第一眼想到的解决方案 是一个时空复杂度均为O(n)的方法
+    {
+        Node* cur = head;
+        vector<Node*> Newlist;
+        map<Node*,int> MAP;
+        int index = 0;
+        while(cur!=nullptr){ //遍历链表，然后建立映射，并把新创的结点放在vector中
+            Node * newnode = new Node(cur->val);
+            MAP[cur] = index++;
+            newnode->random = cur->random;
+            Newlist.push_back(newnode);
+            cur = cur->next;
+        }
+        MAP[nullptr] = Newlist.size();
+        Newlist.push_back(nullptr);
+        for(int i = 0;i<Newlist.size()-1;i++){
+            Newlist[i]->next = Newlist[i+1];
+            Newlist[i]->random = Newlist[MAP[Newlist[i]->random]]; //解开映射
+        }
+        return Newlist[0];
+    }
+    Node * DFS(Node* node,map<Node*,Node*>& MAP) //直接返回新创建的结点
+    { //将复杂链表看成一个图，然后递归的把新new的结点连接
+        if (node==nullptr)
+            return nullptr;
+        if (MAP.find(node)==MAP.end()){
+            Node * newnode = new Node(node->val); 
+            //如果从未访问过，则新建一个结点，并建立旧地址与新地址的映射，以便之后能直接判断有没有访问过
+            MAP[node] = newnode;
+        }else{
+            return MAP[node]; //如果访问过则直接返回映射的新结点
+        }
+        
+        MAP[node]->next = DFS(node->next,MAP);
+        MAP[node]->random = DFS(node->random,MAP);
+        return MAP[node]; //返回映射的新节点，这个结点把next和random也在新的结点域中弄好了
+    }
+    Node* DFSrecur(Node* head)
+    {
+        map<Node*,Node*> MAP;
+        return DFS(head,MAP);
+    }
+
+    Node* NoMapMethod(Node * head) //不用映射的方法 
+    {
+        if (head==nullptr)
+            return nullptr;
+        Node* cur = head;
+        while(cur!=nullptr){
+            Node * curnext = cur->next;
+            Node* newnode = new Node(cur->val);
+            newnode->next = cur->next; //就是把新new的结点插入到旧链表中
+            cur->next = newnode;
+            cur = curnext;
+        }
+        cur = head;
+        while(cur!=nullptr){
+            Node* copyNode = cur->next;
+            copyNode->random = nullptr;
+            if (cur->random!=nullptr)
+                copyNode->random = cur->random->next;
+            cur = copyNode->next;
+        }
+        
+        Node* newhead = head->next;
+        cur = head;
+        int i = 0;
+        while(cur!=nullptr){ //把连起来的链表解链
+            Node* copyNode = cur->next;
+            
+            cur->next = copyNode==nullptr?nullptr:copyNode->next;
+            copyNode->next = cur->next==nullptr?nullptr:cur->next->next; //注意这个边界条件
+            //如果不用冒号表达式的话写起来很复杂
+            cur = cur->next;
+        }
+        return newhead;
+    }
+    Node* copyRandomList(Node* head) {
+        return NoMapMethod(head); //这个空间复杂度是O(1)，时间复杂是O(n)
+        return FirstThought(head);//第一眼想到的，最简单的建立一个vector,然后映射
+        return DFSrecur(head); //将链表看成图，然后用图的遍历方法新建结点
+    }
+};
+// https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/fu-zhi-dai-sui-ji-zhi-zhen-de-lian-biao-by-leetcod/
+
+
+
