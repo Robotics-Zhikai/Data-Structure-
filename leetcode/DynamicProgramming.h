77324627ca8233595589ae1717ed685433009fc6
LAPTOP\zhikaiwang <979256120@qq.com>
2021-05-31 17:22:17 +0800

    LeetCode198 打家劫舍 动态规划

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -933,3 +933,30 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+
+
+/*
+198. 打家劫舍
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+
+*/
+
+class Solution198 {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),0); //标号为i及i之前的最高金额
+        if (nums.size()==1)
+            return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i<nums.size();i++){
+            dp[i] = max(dp[i-2]+nums[i],dp[i-1]);
+            //要不偷第i家，结果是dp[i-2]+nums[i]；要不不偷第i家，结果是dp[i-1]
+            //考虑上边的递归式，可以把空间复杂度优化到O(1).具体的实现方法就是设置两个index，每轮循环(index++)%3
+        }
+        return dp[nums.size()-1];
+    }
+};
