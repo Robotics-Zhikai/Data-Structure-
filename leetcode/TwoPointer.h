88376b20c11e5e52a7f3706c17fe139e8f238184
Robotics-Zhikai <979256120@qq.com>
2021-07-05 23:55:35 +0800

    LeetCode31 下一个排列 双指针

--- leetcode/TwoPointer.h
+++ leetcode/TwoPointer.h
@@ -451,3 +451,65 @@ public:
         //暂时还不知为何
     }
 };
+
+
+/*
+31. 下一个排列
+实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+必须 原地 修改，只允许使用额外常数空间。
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：nums = [3,2,1]
+输出：[1,2,3]
+示例 3：
+
+输入：nums = [1,1,5]
+输出：[1,5,1]
+示例 4：
+
+输入：nums = [1]
+输出：[1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-permutation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//如果不做的话可能面试时候想不到，因此要多复习这里的思路
+class Solution31 {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index = -1;
+        for(int i = nums.size()-2;i>=0;i--){
+            if (nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        } //逆序找到第一个降序的数
+        if (index!=-1){
+            for (int i = nums.size()-1;i>index;i--){
+                if (nums[i]>nums[index]){
+                    swap(nums[i],nums[index]);
+                    break;
+                }
+            } //如果找到那么把右边的"第一次”大于它的数进行交换
+        }
+        
+        int l = index+1;
+        int r = nums.size()-1;
+        while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        } //最后把右半段逆序排列
+        
+    }
+};
