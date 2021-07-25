8be1ab1061a073ff3950ecbe5017a84e450235eb
LAPTOP\zhikaiwang <979256120@qq.com>
2021-07-25 16:37:57 +0800

    LeetCode160 相交链表

--- leetcode/LinkedList.h
+++ leetcode/LinkedList.h
@@ -635,3 +635,43 @@ private:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+
+
+
+/*
+160. 相交链表
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+图示两个链表在节点 c1 开始相交：
+
+
+
+题目数据 保证 整个链式结构中不存在环。
+
+注意，函数返回结果后，链表必须 保持其原始结构 。
+
+
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution160 {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* cur1 = headA;
+        ListNode* cur2 = headB;
+        while(cur1!=cur2){
+            cur1 = cur1==nullptr?headB:cur1->next;
+            cur2 = cur2==nullptr?headA:cur2->next;
+        }
+        return cur1;
+    }
+};
