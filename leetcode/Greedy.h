fa8c0ccd4630d2e6c65c1ec0f61c6f63129bb11f
LAPTOP\zhikaiwang <979256120@qq.com>
2021-07-27 15:18:31 +0800

    LeetCode45 跳跃游戏II 贪心算法

--- leetcode/Greedy.h
+++ leetcode/Greedy.h
@@ -69,3 +69,81 @@ public:
 		return solveMethod3(nums);
     }
 };
+
+/*
+45. 跳跃游戏 II
+给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+假设你总是可以到达数组的最后一个位置。
+
+*/
+
+class Solution {
+public:
+	int solveMethod1(vector<int>&nums){
+		//用这个二维dp直接超时了，但是正确性是可以保证的
+		//这是n^3的时间复杂度
+		vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),INT_MAX)); //dp[i][j]表示从i调到j的最少跳跃次数
+		for(int i = 0;i<nums.size();i++){
+			dp[i][i] = 0;
+		}
+		for(int len = 1;len<nums.size();len++){
+			for(int i = 0;i<nums.size()-len;i++){
+				if (nums[i]>=len){
+					dp[i][i+len] = 1;
+				}
+				else{
+					dp[i][i+len] = INT_MAX;
+				}
+				
+				for(int j = i;j<i+len;j++){
+					if (dp[i][j]!=INT_MAX && dp[j][i+len]!=INT_MAX){
+						dp[i][i+len] = min(dp[i][i+len],dp[i][j]+dp[j][i+len]);
+					}
+				}
+			}
+		}
+		return dp[0][nums.size()-1];
+	}
+	int solveMethod2(vector<int>&nums){
+		//这是n^2的时间复杂度
+		vector<int> dp(nums.size(),INT_MAX);//dp[i]表示从0跳到i的最少跳跃次数
+		dp[0] = 0;
+		for(int i = 1;i<dp.size();i++){
+			for(int j = 0;j<i;j++){
+				if (j+nums[j]>=i){
+					dp[i] = min(dp[i],dp[j]+1);
+				}
+			}
+		}
+		return dp.back();
+	}
+	int solveMethod3(vector<int>& nums){
+		//应该是有n的时间复杂度的贪心算法
+        //太难写了！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+		int beginindex = 0;
+		int maxindex = 0;
+		int upper = 0;
+		int res = 0;
+		//在遍历数组时，我们不访问最后一个元素，这是因为在访问最后一个元素之前，
+		//我们的边界一定大于等于最后一个位置，否则就无法跳到最后一个位置了。
+		//如果访问最后一个元素，在边界正好为最后一个位置的情况下，
+		//我们会增加一次「不必要的跳跃次数」，因此我们不必访问最后一个元素。
+		for(int i = 0;i<nums.size()-1;i++){
+			maxindex = max(i+nums[i],maxindex);
+			if (i==upper){
+				res++;
+				upper = maxindex;
+			}
+		}
+		return res;
+	}
+    int jump(vector<int>& nums) {
+		return solveMethod3(nums);
+        return solveMethod2(nums);
+    }
+};
