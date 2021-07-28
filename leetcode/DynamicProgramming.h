7c619c31d9f6301acc8aa71163561e74c7968acb
LAPTOP\zhikaiwang <979256120@qq.com>
2021-07-28 23:16:09 +0800

    剑指offer60 n个骰子的点数 动态规划

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -1806,3 +1806,37 @@ public:
 		return dp[m-1][n-1];
     }
 };
+
+/*
+剑指 Offer 60. n个骰子的点数
+把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
+
+ 
+
+你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。
+
+
+*/
+
+class SolutionOffer60 {
+public:
+    vector<double> dicesProbability(int n) {
+        //空间复杂度可以降到n
+        //时间复杂度是n^2
+        vector<vector<double>> dp(n+1,vector<double>(6*n+1,0));
+
+        for(int i = 1;i<=6;i++){
+            dp[1][i] = 1.0/6.0;
+        }
+        for (int i = 2;i<=n;i++){
+            for (int j = i;j<=6*i;j++){
+                for (int k = 1;k<=6;k++){
+                    if (j-k>=0){
+                        dp[i][j]+=dp[i-1][j-k]*(1.0/6.0);
+                    }
+                }
+            }
+        }
+        return vector<double>(dp[n].begin()+n,dp[n].end());
+    }
+};
