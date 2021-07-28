07148dcb4dc023052f5ea1efda14ef70622fb0f9
LAPTOP\zhikaiwang <979256120@qq.com>
2021-07-28 23:35:51 +0800

    剑指offer66 构建乘积数组 动态规划

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -1268,6 +1268,28 @@ public:
     }
 };
 
+/*
+剑指 Offer 66. 构建乘积数组
+给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，
+其中 B[i] 的值是数组 A 中除了下标 i 以外的元素的积, 即 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+
+*/
+class SolutionOffer66 {
+public:
+    vector<int> constructArr(vector<int>& a) {
+        vector<int> res(a.size(),1);
+        for(int i = 1;i<res.size();i++){
+            res[i] = res[i-1]*a[i-1];
+        }
+        int S = 1;
+        for(int i = res.size()-2;i>=0;i--){
+            S = S*a[i+1];
+            res[i] = res[i]*S;
+        }
+        return res;
+    }
+};
+
 
 /*
 279. 完全平方数
