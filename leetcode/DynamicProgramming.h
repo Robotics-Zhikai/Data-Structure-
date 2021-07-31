5c020467c909e91c8726a1fba0926f1dcb737243
LAPTOP\zhikaiwang <979256120@qq.com>
2021-07-31 15:58:08 +0800

    剑指offer10-II 青蛙跳台阶 动态规划 大数求余

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -1999,3 +1999,33 @@ public:
         return solveMethod2(n);
     }
 };
+
+/*
+剑指 Offer 10- II. 青蛙跳台阶问题
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+*/
+class SolutionOffer10_II {
+public:
+    int numWays(int n) {
+        if (n==0){
+            return 1;
+        }
+        else if (n==1){
+            return 1;
+        }
+        else if (n==2){
+            return 2;
+        }
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            dp[i] = ((dp[i-1]%1000000007)+(dp[i-2]%1000000007))%1000000007;
+        }
+        return dp[n];
+    }
+};
