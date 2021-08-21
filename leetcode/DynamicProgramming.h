8e2bdb9afd7ed3ef3e503a44add9d3fafeae2a81
Robotics-Zhikai <979256120@qq.com>
2021-08-21 16:12:32 +0800

    LeetCode53 最大子序和

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -2037,3 +2037,27 @@ public:
         return dp[n];
     }
 };
+
+
+/*
+53. 最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+*/
+class Solution53 {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        dp[0] = nums[0];
+        for(int i = 1;i<nums.size();i++){
+            dp[i] = max(dp[i-1]+nums[i],nums[i]);
+        }
+        int res = INT_MIN;
+        for(int i = 0;i<dp.size();i++){
+            if (dp[i]>res){
+                res = dp[i];
+            }
+        }
+        return res;
+    }
+};
