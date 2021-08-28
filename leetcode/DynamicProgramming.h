310fac03ad4de669aac4caa26f662dbd48603273
Robotics-Zhikai <979256120@qq.com>
2021-08-28 23:01:49 +0800

    LeetCode1143 最长公共子序列 动态规划

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -1007,6 +1007,30 @@ public:
 
 class Solution300 {
 public:
+    int review(vector<int>& nums){
+        vector<int> dp(nums.size(),1);
+        dp[0] = 1;
+        for(int i = 1;i<nums.size();i++){
+            int MAX = INT_MIN;
+            for(int j = 0;j<i;j++){
+                if (nums[i]>nums[j]){
+                    if (dp[j]+1>MAX){
+                        MAX = dp[j]+1;
+                    }
+                }
+            }
+            if (MAX!=INT_MIN){
+                dp[i] = MAX;
+            }
+        }
+        int res = INT_MIN;
+        for(int e:dp){
+            if (e>res){
+                res = e;
+            }
+        }
+        return res;
+    }
     int lengthOfLIS(vector<int>& nums) {
         vector<int> dp(nums.size(),0); //储存包含i位置元素的i位置及之前元素的最长严格递增子序列长度 
         dp[0] = 1;
@@ -2560,5 +2584,55 @@ public:
             }
         }
         return dp[target];
+    }
+};
+
+
+/*
+1143. 最长公共子序列
+给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符
+（也可以不删除任何字符）后组成的新字符串。
+
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+
+
+*/
+class Solution1143 {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),0));
+        for(int i = 0;i<text1.size();i++){
+            if (text1[i]==text2[0]){
+                dp[i][0] = 1;
+            }
+            else{
+                if (i>0)
+                    dp[i][0] = dp[i-1][0];
+            }
+        }
+        for(int i = 0;i<text2.size();i++){
+            if (text1[0]==text2[i]){
+                dp[0][i] = 1;
+            }
+            else{
+                if (i>0)
+                    dp[0][i] = dp[0][i-1];
+            }
+        }
+        
+        for(int i = 1;i<text1.size();i++){
+            for(int j = 1;j<text2.size();j++){
+                if (text1[i]==text2[j]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.size()-1][text2.size()-1];
     }
 };
