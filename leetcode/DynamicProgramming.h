c13ab223e530d6dd86dd6c1a416ce6fa1aa0f7ee
Robotics-Zhikai <979256120@qq.com>
2021-08-29 15:59:22 +0800

    LeetCode392 判断子序列

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -2792,3 +2792,90 @@ public:
 
     }
 };
+
+/*
+392. 判断子序列
+给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+进阶：
+
+如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。
+在这种情况下，你会怎样改变代码？
+
+致谢：
+
+特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+*/
+
+class Solution392 {
+public:
+    bool solveMethod1(string s,string t){
+        int ptr = 0;
+        for(int i = 0;i<t.size();i++){
+            if (t[i]==s[ptr]){
+                ptr++;
+            }
+        }
+        if (ptr==s.size()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool solveMethod2(string s,string t){
+        if (t.empty() && !s.empty()){
+            return 0;
+        }
+        if (s.empty()){
+            return 1;
+        }
+        vector<vector<int>> dp(s.size(),vector<int>(t.size(),0));
+        //dp[i][j]表示s[0,i]和t[0,j]的最长公共子序列
+        for(int i = 0;i<t.size();i++){
+            if (s[0]==t[i]){
+                dp[0][i] = 1;
+            }
+            else{
+                if (i>0){
+                    dp[0][i] = dp[0][i-1];
+                }
+            }
+        }
+        for(int i = 0;i<s.size();i++){
+            if (s[i]==t[0]){
+                dp[i][0] = 1;
+            }
+            else{
+                if (i>0){
+                    dp[i][0] = dp[i-1][0];
+                }
+            }
+        }
+
+        for(int i = 1;i<s.size();i++){
+            for(int j = 1;j<t.size();j++){
+                if (s[i]==t[j]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        if (dp[s.size()-1][t.size()-1]==s.size()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    //对于进阶的思考题，可以采用空间换时间的做法，先扫一遍t，然后把t中的每个字符所处的位置
+    //映射到链表中，然后每个字符串S都能以length(S)的复杂度去判断了（前提是字符分布均匀）
+    bool isSubsequence(string s, string t) {
+        return solveMethod2(s,t);
+    }
+};
