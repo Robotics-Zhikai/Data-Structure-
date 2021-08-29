b6b450b21f9cedcbee4fa2ee0bf11a0ebac01065
Robotics-Zhikai <979256120@qq.com>
2021-08-29 22:19:51 +0800

    剑指offer25 合并两个排序的链表

--- leetcode/LinkedList.h
+++ leetcode/LinkedList.h
@@ -1023,3 +1023,38 @@ public:
         return res;
     }
 };
+
+/*
+剑指 Offer 25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class SolutionOffer25 {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1==nullptr && l2==nullptr){
+            return nullptr;
+        }
+        else if (l1==nullptr){
+            return l2;
+        }
+        else if (l2==nullptr){
+            return l1;
+        }
+        if (l1->val<=l2->val){
+            l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else {
+            l2->next = mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+};
