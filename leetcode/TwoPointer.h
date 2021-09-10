9eeb3dfc2e8970bfbd8d74aa55cce6cf601d3c64
Robotics-Zhikai <979256120@qq.com>
2021-09-10 21:14:10 +0800

    剑指offer57 和为s的两个数字

--- leetcode/TwoPointer.h
+++ leetcode/TwoPointer.h
@@ -589,3 +589,28 @@ public:
         solveMethod2(nums);
     }
 };
+
+/*
+剑指 Offer 57. 和为s的两个数字
+输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+
+*/
+class SolutionOffer57 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<right){
+            while (left<right && nums[left]+nums[right]>target){
+                right--;
+            }
+            if (left<right && nums[left]+nums[right]==target){
+                return vector<int>{nums[left],nums[right]};
+            }
+            while (left<right && nums[left]+nums[right]<target){
+                left++;
+            }
+        }
+        return {};
+    }
+};
