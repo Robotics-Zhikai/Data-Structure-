926f0b6d80ea0999ef86e27d1919caf0c17c9d2a
Robotics-Zhikai <979256120@qq.com>
2021-09-11 23:44:46 +0800

    剑指offer39 数组中出现次数超过一半的数字

--- leetcode/Stack_Queue.h
+++ leetcode/Stack_Queue.h
@@ -646,3 +646,37 @@ public:
         }
     }
 };
+
+
+/*
+剑指 Offer 39. 数组中出现次数超过一半的数字
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+*/
+class SolutionOffer39 {
+public:
+    /*
+摩尔投票法找的其实不是众数，而是占一半以上的数。当数组没有超过一半的数，则可能返回非众数，比如[1, 1, 2, 2, 2, 3, 3]，最终返回3。
+
+投票法简单来说就是不同则抵消，占半数以上的数字必然留到最后 可以用一个栈实现这个
+
+    */
+    int majorityElement(vector<int>& nums) {
+        stack<int> STA;
+        for(int i = 0;i<nums.size();i++){
+            if (STA.empty()){
+                STA.push(nums[i]);
+            }
+            else{
+                if (nums[i]!=STA.top()){
+                    STA.pop();
+                }
+                else{
+                    STA.push(nums[i]);
+                }
+            }
+        }
+        return STA.top();
+    }
+};
