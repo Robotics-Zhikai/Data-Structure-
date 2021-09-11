7486b727d6d3f81f2f857a93dad56154e68b4ee3
Robotics-Zhikai <979256120@qq.com>
2021-09-11 15:22:47 +0800

    剑指offer68 II 二叉树的最近公共祖先

--- leetcode/Tree.h
+++ leetcode/Tree.h
@@ -1491,3 +1491,39 @@ public:
         }
     }
 };
+
+
+/*
+剑指 Offer 68 - II. 二叉树的最近公共祖先
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+*/
+
+class SolutionOffer68II {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root==nullptr){
+            return nullptr;
+        }
+        if (root==p || root==q){
+            return root;
+        }
+        TreeNode* node1;
+        TreeNode* node2;
+        node1 = lowestCommonAncestor(root->left,p,q);
+        node2 = lowestCommonAncestor(root->right,p,q);
+        if (node1 !=nullptr && node2!=nullptr){
+            return root;
+        }
+        if (node1!=nullptr){
+            return node1;
+        }
+        if (node2!=nullptr){
+            return node2;
+        }
+        return nullptr;
+    }
+};
