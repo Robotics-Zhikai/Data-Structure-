db121c78178b666950914fb775ac0f22111a72be
Robotics-Zhikai <979256120@qq.com>
2021-09-11 14:12:52 +0800

    剑指offer52 两个链表的第一个公共节点

--- leetcode/LinkedList.h
+++ leetcode/LinkedList.h
@@ -1058,3 +1058,39 @@ public:
         }
     }
 };
+
+/*
+剑指 Offer 52. 两个链表的第一个公共节点
+输入两个链表，找出它们的第一个公共节点。
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class SolutionOffer52 {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        while(curA!=curB){
+            if (curA!=nullptr){
+                curA = curA->next;
+            }
+            else{
+                curA = headB;
+            }
+            if (curB!=nullptr){
+                curB = curB->next;
+            }
+            else{
+                curB = headA;
+            }
+        }
+        return curA;
+    }
+};
