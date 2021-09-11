3d0799c9c4fa9700a5a12faf2dd970b578ca4ca7
Robotics-Zhikai <979256120@qq.com>
2021-09-11 14:06:03 +0800

    剑指offer58-I 翻转单词顺序

--- leetcode/TwoPointer.h
+++ leetcode/TwoPointer.h
@@ -614,3 +614,38 @@ public:
         return {};
     }
 };
+
+/*
+剑指 Offer 58 - I. 翻转单词顺序
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+*/
+class SolutionOffer58I {
+public:
+    string reverseWords(string s) {
+        int right = s.size()-1;
+        int left = right;
+        vector<string> vecstr;
+        while(left>=0 && right>=0){
+            while(right>=0 && s[right]==' '){
+                right--;
+                left--;
+            }
+            while(left>=0 && s[left]!=' '){
+                left--;
+            }
+            if (right>=0)
+                vecstr.push_back(s.substr(left+1,right-left));
+            right = left;
+        }
+        string res;
+        for(auto str:vecstr){
+            for(char c:str){
+                res.push_back(c);
+            }
+            res.push_back(' ');
+        }
+        res.pop_back();
+        return res;
+    }
+};
