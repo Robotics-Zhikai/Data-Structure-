8ebe328c8dd7b0621b259b0496d73a6b13cb6238
Robotics-Zhikai <979256120@qq.com>
2021-09-15 22:17:58 +0800

    LeetCode64 最小路径和 review

--- leetcode/DynamicProgramming.h
+++ leetcode/DynamicProgramming.h
@@ -376,6 +376,23 @@ public:
 //这道题只能向下或者向右移动一步 是一个比较明显的动态规划 但是当上下左右都能移动时，就不好看成动态规划了 可能是贪心
 class Solution64 {
 public:
+    int review(vector<vector<int>>& grid){
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1;i<grid.size();i++){
+            dp[i][0] = dp[i-1][0]+grid[i][0];
+        }
+        for(int j = 1;j<grid[0].size();j++){
+            dp[0][j] = dp[0][j-1]+grid[0][j];
+        }
+        for(int i = 1;i<grid.size();i++){
+            for(int j = 1;j<grid[i].size();j++){
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[grid.size()-1][grid[0].size()-1];
+    }
+    
 	int inf = 99999;
 	int minPathSum(vector<vector<int>>& grid) {
 		if (grid.empty())
