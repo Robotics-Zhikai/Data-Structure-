b4a85025b757afa424fe4c2a74cce6bf030d2cd6
Robotics-Zhikai <979256120@qq.com>
2021-09-16 10:28:23 +0800

    LeetCode71 简化路径

--- leetcode/Stack_Queue.h
+++ leetcode/Stack_Queue.h
@@ -761,3 +761,67 @@ public:
 private:
     queue<int> que;
 };
+
+
+/*
+71. 简化路径
+给你一个字符串 path ，表示指向某一文件或目录的 Unix 风格 绝对路径 （以 '/' 开头），请你将其转化为更加简洁的规范路径。
+
+在 Unix 风格的文件系统中，一个点（.）表示当前目录本身；此外，两个点 （..） 表示将目录切换到上一级（指向父目录）；
+两者都可以是复杂相对路径的组成部分。任意多个连续的斜杠（即，'//'）都被视为单个斜杠 '/' 。 
+对于此问题，任何其他格式的点（例如，'...'）均被视为文件/目录名称。
+
+请注意，返回的 规范路径 必须遵循下述格式：
+
+始终以斜杠 '/' 开头。
+两个目录名之间必须只有一个斜杠 '/' 。
+最后一个目录名（如果存在）不能 以 '/' 结尾。
+此外，路径仅包含从根目录到目标文件或目录的路径上的目录（即，不含 '.' 或 '..'）。
+返回简化后得到的 规范路径 。
+
+*/
+class Solution71 {
+public:
+    string simplifyPath(string path) {
+        vector<string> splitvecstr;
+
+        int ptr = 0;
+        while(ptr<path.size()){
+            while(ptr<path.size() && path[ptr]=='/'){
+                ptr++;
+            }
+            string str;
+            while(ptr<path.size() && path[ptr]!='/'){
+                str.push_back(path[ptr]);
+                ptr++;
+            }
+            if (!str.empty())
+                splitvecstr.push_back(str);
+        } //先把用/间隔的string提取出来，存放到一个vector里
+
+        string res ;
+        for(string & str:splitvecstr){
+            if (str==string(".")){
+                continue;
+            }
+            else if (str==string("..")){
+                if (!res.empty()){ //这里容易出错
+                    while(*(res.end()-1)!='/'){
+                        res.pop_back();
+                    }
+                    res.pop_back(); //弹出'/'
+                }
+                continue;
+            }
+            
+            res.push_back('/');
+            for(char c:str){
+                res.push_back(c);
+            }
+        } //尽量要搞一个固定的范式
+        if (res.empty()){ //这里容易出错
+            res.push_back('/');
+        }
+        return res;        
+    }
+};
