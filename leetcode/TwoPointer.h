72c249826381f0ee34313307bb3dd806d9b85513
Robotics-Zhikai <979256120@qq.com>
2021-09-24 19:12:11 +0800

    LeetCode443 压缩字符串

--- leetcode/TwoPointer.h
+++ leetcode/TwoPointer.h
@@ -843,3 +843,49 @@ public:
         return res;
     }
 };
+
+
+/*
+443. 压缩字符串
+给你一个字符数组 chars ，请使用下述算法压缩：
+
+从一个空字符串 s 开始。对于 chars 中的每组 连续重复字符 ：
+
+如果这一组长度为 1 ，则将字符追加到 s 中。
+否则，需要向 s 追加字符，后跟这一组的长度。
+压缩后得到的字符串 s 不应该直接返回 ，需要转储到字符数组 chars 中。需要注意的是，
+如果组长度为 10 或 10 以上，则在 chars 数组中会被拆分为多个字符。
+
+请在 修改完输入数组后 ，返回该数组的新长度。
+
+你必须设计并实现一个只使用常量额外空间的算法来解决此问题
+
+*/
+class Solution443 {
+public:
+    int compress(vector<char>& chars) {
+        int valid = 0;
+        int cur = 0;
+
+        while(cur<chars.size()){
+            char curchar = chars[cur];
+            int count = 0;
+            while(cur<chars.size() && chars[cur]==curchar){
+                count++;
+                cur++;
+            }
+            string charNum = to_string(count);
+            chars[valid++] = curchar;
+            if (count!=1){
+                for(char c:charNum){
+                    chars[valid++] = c;
+                }
+            }
+        }
+        int popsize = chars.size()-valid;
+        for(int i = 0;i<popsize;i++){
+            chars.pop_back();
+        }
+        return valid;
+    }
+};
