a49d5b5d27df1b00fe249892388170880b9e873c
Robotics-Zhikai <979256120@qq.com>
2021-09-25 14:04:55 +0800

    LeetCode25 K个一组翻转链表 三指针

--- leetcode/LinkedList.h
+++ leetcode/LinkedList.h
@@ -1094,3 +1094,61 @@ public:
         return curA;
     }
 };
+
+
+
+
+/*
+25. K 个一组翻转链表
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+进阶：
+
+你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+*/
+
+class Solution25 {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* cur1 = head;
+        ListNode* PrevLAST = nullptr;
+        ListNode* PrevFIRST = nullptr;
+        
+        while(cur1!=nullptr){
+            ListNode* cur2 = cur1;
+            int num = 0;
+            while(cur1!=nullptr && num<k){
+                num++;
+                cur1 = cur1->next;
+            } //cur1先提前看看是否能够积累k个去翻转
+            if (num==k){
+                ListNode* cur3 = cur2->next;
+                ListNode* sotrage = PrevLAST;
+                PrevLAST = cur2;
+                cur2->next = nullptr; //注意这个一开始就要置为nullptr
+                while(cur3!=cur1){
+                    ListNode* tmp = cur3->next;
+                    cur3->next = cur2;
+                    cur2 = cur3;
+                    cur3 = tmp;
+                } //如果可以的话就挨个翻转
+                if (sotrage!=nullptr)
+                    sotrage->next = cur2;
+                if (PrevFIRST==nullptr){
+                    PrevFIRST = cur2;
+                }
+            }
+            else{
+                if (PrevLAST!=nullptr){
+                    PrevLAST->next = cur2;
+                }
+            }
+        }
+        return PrevFIRST;
+    }
+};
